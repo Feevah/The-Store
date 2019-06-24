fbf8c9b5b7e22eb649d123103768436c30cd7e53
Feevah <cddefever@gmail.com>
2019-06-24 19:29:42 -0400

    clean

--- store.js
+++ store.js
@@ -1,5 +1,5 @@
 "use strict"
-
+setTimeout(function(){
 // Exercise
 
 // Create an inventory array and associate a number with each item. Then get user input that purchases items (subtracts one from the item).
@@ -15,49 +15,7 @@
 // inventory count
 
 
-
-
-
-
-
-// method 1 ------------------------------------------------------------
-
-// var input = prompt("What would you like to order")
-
-// var Blueberries = {name: "Blueberries", amount: 40};
-// var Kiwis = {name: "Kiwis", amount: 40};
-// var Oranges = {name: "Oranges", amount: 40};
-// var Peaches = {name: "Peaches", amount: 40};
-// var Mangoes = {name: "Mangoes", amount: 40};
-// var Guavas = {name: "Guavas", amount: 40};
-// var Strawberries = {name: "Strawberries", amount: 40};
-// var Cucumbers = {name: "Cucumbers", amount: 40};
-// var Squash = {name: "Squash", amount: 40};
-// var Broccoli = {name: "Broccoli", amount: 40};
-
-// var items = ["Blueberries", "Kiwis", "Oranges", "Peaches", "Mangoes", "Guavas", "Strawberries", "Cucumbers", "Squash", "Broccoli"];
-// var quantity = [Blueberries, Kiwis, Oranges, Peaches, Mangoes, Guavas, Strawberries, Cucumbers, Squash, Broccoli]
-
-// var order = function (input) {
-//      if (input === quantity) {
-//      var orderNumber = Number(prompt ("We have " + Blueberries + ". How many do you want?"));
-//      var newQuant = Blueberries - orderNumber;
-//      alert("Your order has been placed. There are " + newQuant + " left");
-
-//   }
-
-// }
-
-
-
-
-
-
-
-// // method 2 ----------------------------
-
-
-var input = prompt("What would you like to order")
+var input = prompt("What would you like to order\nWe have the Following items:\n" + items)
 var items = [["Blueberries", 10], 
 			 ["Kiwis", 20], 
 			 ["Oranges", 50], 
@@ -68,9 +26,6 @@ var items = [["Blueberries", 10],
 			 ["Cucumbers", 56], 
 			 ["Squash", 29], 
 			 ["Broccoli", 18]];
-
-// var items = ["Blueberries", "Kiwis", "Oranges", "Peaches", "Mangoes", "Guavas", "Strawberries", "Cucumbers", "Squash", "Broccoli"];
-// var quantity = [10, 7, 20, 50, 7, 56, 7 , 43, 10, 87];
 	
 
 	var i = 0;
@@ -101,44 +56,7 @@ var items = [["Blueberries", 10],
 
 
 
-// method 3 ------------------------------------------------
-
-
-// var input = prompt("What would you like to order")
-// var items = [["Blueberries", 10], 
-// 			 ["Kiwis", 20], 
-// 			 ["Oranges", 50], 
-// 			 ["Peaches", 45], 
-// 			 ["Mangoes", 9], 
-// 			 ["Guavas", 29], 
-// 			 ["Strawberries", 104], 
-// 			 ["Cucumbers", 56], 
-// 			 ["Squash", 29], 
-// 			 ["Broccoli", 18]];
-
-//      if (input === items[0][0]) {
-//      var orderNumber = Number(prompt ("We have " + items[0][1] + ". How many do you want?"));
-//      var newQuant = items[0][1] - orderNumber;
-//      alert("Your order has been placed. There are " + newQuant + " left");
-// 	}
-
-// 	if (newQuant < 5) {
-// 		var placeOrder = Number(prompt("We only have " + newQuant + ". How much should I order?"))
-//         var newAmount = newQuant + placeOrder;
-//         alert("Your order has been placed. There are " + newAmount + " left");
-//         quantity = newAmount;
-
-// 	}
-
-// 	if (placeOrder === 0 || placeOrder === null) {
-// 	   var doubleCheck = Number(prompt("Are you sure that you don't want to order? Enter amount or press cancel to escape."))
-
-// 	}
-
-
-// console.log (items.length);
-// console.log (items);
-
+}, 1000);
 
 
 // while (items.length <= items) {console.log (items);}
