c0bfa70faa9e3c1691f476c9feefc97c657ffadb
Feevah <cddefever@gmail.com>
2019-09-08 19:55:10 -0400

    DOM

--- store.js
+++ store.js
@@ -1,21 +1,97 @@
 "use strict"
+
+var date = new Date ();
+var today = date.getDate();
+var month = date.getMonth() + 1;
+var year = date.getFullYear();
+
+document.querySelector(".date").innerHTML = (month + '-' + today + '-' + year);
+
+
+var items = [["Blueberries", 40], 
+			 ["Kiwis", 20], 
+			 ["Oranges", 50], 
+			 ["Peaches", 45], 
+			 ["Mangoes", 37], 
+			 ["Guavas", 26], 
+			 ["Strawberries", 104], 
+			 ["Cucumbers", 56], 
+			 ["Squash", 29], 
+			 ["Broccoli", 18]];
+
+// The amounts
+document.querySelector(".bbCount").textContent= items[0][1]
+document.querySelector(".kCount").textContent= items[1][1]
+document.querySelector(".oCount").textContent= items[2][1]
+document.querySelector(".pCount").textContent= items[3][1]
+document.querySelector(".mCount").textContent= items[4][1]
+document.querySelector(".gCount").textContent= items[5][1]
+document.querySelector(".sCount").textContent= items[6][1]
+document.querySelector(".cCount").textContent= items[7][1]
+document.querySelector(".sqCount").textContent= items[8][1]
+document.querySelector(".bCount").textContent= items[9][1]	
+
+
+
+
+
+
+
+// <============================== The Prompt ===============================>
+
+
 setTimeout(function(){
-    var date = new Date ();
-	var today = date.getDate();
-	var month = date.getMonth() + 1;
-	var year = date.getFullYear();
-	// var trip = new Date ();
-	// trip.setTime(date.getTime);
-	// var day = date.getTime();
-	// var time = date.getFullYear();
-    document.querySelector(".date").innerHTML = (month + '-' + today + '-' + year );
+// The loop & propmpt
 
-
-}, 100);
+var input = prompt("What would you like to order?\nWe have the items behind this alert:\nPlease make first letter of item capitalized\n(i.e. Kiwis not kiwis)");
+	var i = 0;
 
 
 
-setTimeout(function(){
+// Choosing what you want
+
+	 while (input !== null){
+	 	items[i][0]; 
+
+	 	if (input === items[i][0]) {
+     	var orderNumber = Number(prompt ("We have " + items[i][0, 1] + ". How many do you want?"));
+     	var newQuant = items[i][0,1] - orderNumber;
+     	alert("Your order has been placed. There are " + newQuant + " left");
+	    }
+     
+
+	    if (newQuant < 10) {
+		var placeOrder = Number(prompt("We only have " + newQuant + ". How much should I order?"))
+        var newAmount = newQuant + placeOrder;
+        alert("Your order has been placed. There are " + newAmount + " left");
+        quantity = newAmount;
+
+	    }
+
+	    else if (placeOrder === 0 || placeOrder === null) {
+	   var doubleCheck = (prompt("Are you sure that you don't want to order? Enter amount or press cancel to escape."))
+	    }
+
+	    i++;
+}
+
+
+}, 8000);
+
+
+
+
+
+
+
+
+
+// <======================= Old Stuff Section ==============================>
+
+
+
+// Junk 1
+
 // Exercise
 
 // Create an inventory array and associate a number with each item. Then get user input that purchases items (subtracts one from the item).
@@ -68,107 +144,9 @@ setTimeout(function(){
 // form.pushButton('register', logSubmit);
 
 
-var input = prompt("What would you like to order?\nWe have the items behind this alert:\nPlease make first letter of item capitalized\n(i.e. Kiwis not kiwis)");
-var items = [["Blueberries", 10], 
-			 ["Kiwis", 20], 
-			 ["Oranges", 50], 
-			 ["Peaches", 45], 
-			 ["Mangoes", 9], 
-			 ["Guavas", 29], 
-			 ["Strawberries", 104], 
-			 ["Cucumbers", 56], 
-			 ["Squash", 29], 
-			 ["Broccoli", 18]];
-	
-
-	var i = 0;
-
-	// // Inventory in prompt
- //      while (i < items.length){
- //      	i++;
-
- //      	// Item name
- //      var updateItem = items[i][0];
- //       return items
-
- //        // Item Quantity
- //      var updateQuantity = items[i][1];
-
- //      }
-
-    // Choosing what you want
-
-	 while (input !== null){
-	 	items[i][0]; 
-
-	 	if (input === items[i][0]) {
-     	var orderNumber = Number(prompt ("We have " + items[i][0, 1] + ". How many do you want?"));
-     	var newQuant = items[i][0,1] - orderNumber;
-     	alert("Your order has been placed. There are " + newQuant + " left");
-	    }
-     
-
-	    if (newQuant < 5) {
-		var placeOrder = Number(prompt("We only have " + newQuant + ". How much should I order?"))
-        var newAmount = newQuant + placeOrder;
-        alert("Your order has been placed. There are " + newAmount + " left");
-        quantity = newAmount;
-
-	    }
-
-	    if (placeOrder === 0 || placeOrder === null) {
-	   var doubleCheck = Number(prompt("Are you sure that you don't want to order? Enter amount or press cancel to escape."))
-
-	    }
-
-	    i++;
-}
-
-var today = new Date();
-var year = today.getFullYear();
 
 
-
-// var quantity = document.querySelector
-
-
-// addEvent(quantity, "change", function() {
-// 	if (this.value === "choose") 
-
-
-
-// }
-
-
-
-
-// Button
-    document.querySelector(".placeO").addEventListener("click", function(){ 
-
-	document.querySelector(".placeO").style.backgroundColor = "#8fa854";
-
-	document.querySelector("body").style.backgroundColor = "white";
-
-	document.querySelector("h1").style.color = "blue";
-    var goodEats = "<h4>Click and you will see the good Eats we have available.</h4>";
-
-	document.querySelector(".grid").innerHTML = goodEats;
-
-	
-
-
-
-
-
-
-
-
- });
-
-	
-
-
-}, 10000);
+// Junk 2
 
 // document.querySelector(".placeO").addEventListener("click", function(){
 //   document.querySelector(".grid").x.style.display === "none" 
@@ -178,11 +156,6 @@ var year = today.getFullYear();
 //     x.style.display = "none";
 //   }
 // });
-
-
-
-
-
 
 
 // while (items.length <= items) {console.log (items);}
@@ -208,3 +181,58 @@ var year = today.getFullYear();
 // }
 
 
+
+
+
+// Junk 3
+
+// var quantity = document.querySelector
+
+
+// addEvent(quantity, "change", function() {
+// 	if (this.value === "choose") 
+
+
+
+// }
+
+
+
+// Button
+
+
+
+
+// Junk 4
+
+	// // Inventory in prompt
+ //      while (i < items.length){
+ //      	i++;
+
+ //      	// Item name
+ //      var updateItem = items[i][0];
+ //       return items
+
+ //        // Item Quantity
+ //      var updateQuantity = items[i][1];
+
+ //      }
+
+
+
+ // Playing around 
+
+
+//     document.querySelector(".placeO").addEventListener("click", function(){ 
+
+// 	document.querySelector(".placeO").style.backgroundColor = "#8fa854";
+
+// 	document.querySelector("body").style.backgroundColor = "white";
+
+// 	document.querySelector("h1").style.color = "blue";
+//     var goodEats = "<h4>Click and you will see the good Eats we have available.</h4>";
+
+// 	document.querySelector(".grid").innerHTML = goodEats;
+
+	
+// });
